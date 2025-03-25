e374f65d82ce622ea2b00856d9a0484725553100
Torgeir Holen <torgeir333@gmail.com>
2025-03-25 13:34:12 +0100

    Switch to using tfe_outputs

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,14 @@
 # ta dette vpc id og bruke det til å sette opp en EC2 instans.
 # Nå etter lunch prøver jeg også få ut subnets. Spurte ChatGPT, som gav en blokk.
 
+# 25.03.25: Kurs video 7.11 skal sette tfe_outputs.
+
 output "vpc_id" {
   value = module.main.vpc_id
+  description = "VPC ID"
 }
 
-output "public_subnet_ids" {
+output "public_subnets" {
   value = module.main.public_subnets
+  description = "Public Subnets"
 }
